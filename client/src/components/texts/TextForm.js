8d37cecd7c8c54bfc0400f49d4f8ab4cfabdd701
Arnold <arnoldemmanuel15@gmail.com>
2022-02-27 12:58:02 +0100

    Installed rich text and html parser

--- client/src/components/texts/TextForm.js
+++ client/src/components/texts/TextForm.js
@@ -1,6 +1,9 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, Fragment } from 'react';
 import TextContext from '../contexts/texts/TextContexts';
 import { v4 } from 'uuid';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import parse from 'html-react-parser'
 
 const TextForm = () => {
   const { addText, current, clearCurrent, updateText } =
@@ -37,6 +40,15 @@ const TextForm = () => {
   const onChange = (e) => {
     setText({ ...textstate, [e.target.name]: e.target.value });
   };
+
+  const [txtEditor, setTxtEditor] = useState('')
+
+  const txtHandler = (e, editor) => {
+    const data = editor.getData()
+    setTxtEditor(data)
+    console.log(editor.getData())
+  }
+
   const onSubmit = (e) => {
     e.preventDefault();
     if (current !== null) {
@@ -47,38 +59,45 @@ const TextForm = () => {
     clearAll();
   };
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        className="form-control"
-        type="text"
-        name="title"
-        value={title}
-        placeholder="Enter Title"
-        onChange={onChange}
-      />
-      <textarea
-        className="form-control mt-3"
-        name="textarea"
-        id=""
-        cols="30"
-        value={textarea}
-        rows="10"
-        placeholder="Type here....."
-        onChange={onChange}
-        required
-      ></textarea>
-      <div className="d-grid mt-3">
-        {current === null ? (
-          <button className="bd-success btn btn-success btn-block">
-            Add <i className="fas fa-plus-circle"></i>{' '}
-          </button>
-        ) : (
-          <button className="bd-success btn btn-success btn-block">
-            Update <i className="fas fa-plus-circle"></i>{' '}
-          </button>
-        )}
-      </div>
-    </form>
+    <Fragment>
+      <form onSubmit={onSubmit}>
+        <input
+          className="form-control"
+          type="text"
+          name="title"
+          value={title}
+          placeholder="Enter Title"
+          onChange={onChange}
+        />
+        <textarea
+          className="form-control mt-3"
+          name="textarea"
+          id=""
+          cols="30"
+          value={textarea}
+          rows="10"
+          placeholder="Type here....."
+          onChange={onChange}
+          required
+        ></textarea>
+        <div className="d-grid mt-3">
+          {current === null ? (
+            <button className="bd-success btn btn-success btn-block">
+              Add <i className="fas fa-plus-circle"></i>{' '}
+            </button>
+          ) : (
+            <button className="bd-success btn btn-success btn-block">
+              Update <i className="fas fa-plus-circle"></i>{' '}
+            </button>
+          )}
+        </div>
+      </form>
+      <br />
+      <CKEditor editor={ClassicEditor} onChange={txtHandler} />
+      <hr />
+      <p>{parse(txtEditor)}</p>
+    </Fragment>
+
   );
 };
 
